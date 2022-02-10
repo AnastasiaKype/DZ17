e72d3b431e80673db7332863b098284be22e1983
AnastasiaKype <black_cat123@mail.ru>
2022-02-10 22:40:17 +0300

    первый метод

---
+++ HomeWork17/src/Main.java
@@ -0,0 +1,14 @@
+import static kype.nastya.speedCheckMas.speedCheckMas;
+
+public class Main {
+
+
+    public static void main(int speedCheckMas) {
+        
+        int losePlayers = speedCheckMas(0);
+        System.out.println(losePlayers);
+
+
+    }
+}
+
---
+++ HomeWork17/src/kype/nastya/speedCheckMas.java
@@ -0,0 +1,40 @@
+package kype.nastya;
+
+
+public class speedCheckMas {
+    public static boolean isGreenLight = false;
+
+    public static int speedCheckMas(int numbersOfPlayers) {
+
+        int[] speeds = new int[3];
+
+
+        speeds[0] = 5;
+        speeds[1] = 0;
+        speeds[2] = 2;
+        numbersOfPlayers = 0;
+
+
+        int i = 0;
+
+        if (isGreenLight == true) {
+
+            System.out.println("Выбывших:" + numbersOfPlayers);
+        } else {
+            while (i <= 2) {
+                if (speeds[i] > 0) {
+
+                    numbersOfPlayers = numbersOfPlayers + 1;
+                    i++;
+                } else {
+                    numbersOfPlayers = numbersOfPlayers;
+                    i++;
+                }
+
+            }
+
+        }
+        return numbersOfPlayers;
+
+    }
+}
