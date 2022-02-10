3b24e1f87cfedb627eacbc1326f5c579e95ae3fe
AnastasiaKype <black_cat123@mail.ru>
2022-02-10 22:51:01 +0300

    доработка метода

--- HomeWork17/src/Main.java
+++ HomeWork17/src/Main.java
@@ -3,10 +3,10 @@ import static kype.nastya.speedCheckMas.speedCheckMas;
 public class Main {
 
 
-    public static void main(int speedCheckMas) {
+    public static void main(String[] args) {
         
         int losePlayers = speedCheckMas(0);
-        System.out.println(losePlayers);
+        System.out.println("Выбывших " + losePlayers);
 
 
     }
