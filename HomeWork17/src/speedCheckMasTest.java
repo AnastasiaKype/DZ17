42166bb8da1f998bdf559ddfb109f89d737f9c5c
AnastasiaKype <black_cat123@mail.ru>
2022-02-13 22:55:38 +0300

    DZ17_3

--- HomeWork17/src/speedCheckMasTest.java
+++ HomeWork17/src/speedCheckMasTest.java
@@ -1,3 +1,5 @@
+import kype.nastya.speedCheckMas2;
+import kype.nastya.speedCheckMas3;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +15,28 @@ public class speedCheckMasTest {
         int actual = speedCheckMas(new int[]{5,0,2});
 
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testingTwoMet() {
+        int[] speeds = {5,0,2};
+
+        int[] expected = {5,2};
+        int[] actual = speedCheckMas2.speedCheckLosers(new int[]{5, 0, 2});
+
+        Assertions.assertArrayEquals(expected,actual);
+
+    }
+
+    @Test
+    public void testingThreeMet() {
+        int[] speeds = {5,0,2};
+
+        int[] expected = {0};
+        int[] actual = speedCheckMas3.speedCheckWin(new int[]{5, 0, 2});
+
+        Assertions.assertArrayEquals(expected,actual);
+
     }
 
 }
